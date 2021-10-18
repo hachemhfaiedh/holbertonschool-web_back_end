34d6bfddb4cfcbb7e7641f54c5d2e039ec044ee4
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-10-19 00:01:54 +0200

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float. """
+""" Basic annotations - add """
 
 
 def add(a: float, b: float) -> float:
-    """ type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float. """
+    """ Basic annotations - add """
     return a + b
